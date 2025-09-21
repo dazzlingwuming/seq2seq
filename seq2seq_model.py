16391b9f37dc7a16aeafe29f59025eedafc3d8ad
lihaodong <2409261729@qq.com>
2025-09-21 16:33:16 +0800

    seq2seq实现

--- seq2seq_model.py
+++ seq2seq_model.py
@@ -13,9 +13,9 @@ class Seq2Seq(nn.Module):
     def forward(self, encode_x, decode_x, decode_y):
         #embeding
         embeding_x = self.embeding_layer(encode_x)#[N,T1]->[N,T1,E]
-        decode_x = self.embeding_layer(embeding_x)#[N,T2]->[N,T2,E]
+        decode_x = self.embeding_layer(decode_x)#[N,T2]->[N,T2,E]
         #编码器
-        encode_x, (h_e, c_e) = self.encoder_layer(encode_x)#这里的encode_x是没有什么作用的,h_e[batch, num_layers*num_directions,hidden_size] c_e[batch, num_layers*num_directions,hidden_size]
+        encode_x, (h_e, c_e) = self.encoder_layer(embeding_x)#这里的encode_x是没有什么作用的,h_e[batch, num_layers*num_directions,hidden_size] c_e[batch, num_layers*num_directions,hidden_size]
         #解码器
         decode_x, (h_d, c_d) = self.decoder_layer(decode_x, (h_e, c_e))#decode_x[N.T2,H]
         #全连接层
@@ -34,7 +34,7 @@ class Seq2Seq(nn.Module):
         embeding_x = self.embeding_layer(x)#[N,1]->[N,1,E]
         encode_x, (h_e, c_e) = self.encoder_layer(embeding_x)#encode_x[N,1,H] h_e[1,N,H] c_e[1,N,H]
         #解码器,需要构建一个初始的输入，这里就作为SOS,此外需要循环输出，每次将上一次的输出作为下一次的输入，直到输出最大长度或者输出EOS
-        input_x = torch.tensor([0]*x.size(0), dtype=torch.long).unsqueeze(1).to(x.device)#假设0是SOS,[N,1],decode_x[0] = SOS
+        input_x = torch.tensor([0]*x.size(0), dtype=torch.long).unsqueeze(1).to(x.device)#假设0是SOS,[N,1],decode_x[0] = GO
         #这里需要一个循环
         outputs = []
         for i in range(max_length):
@@ -56,7 +56,7 @@ if __name__ == '__main__':
     x = ["A", "B" , "C", "D"]
     y = ["B", "C", "D", "E"]
     encode_x = torch.tensor([1, 2, 3, 4],dtype = torch.long)#["A", "B" , "C", "D"]
-    decode_x = torch.tensor([0, 2, 3, 4, 5] ,dtype = torch.long)#["<eos>", "B", "C", "D", "E"]
+    decode_x = torch.tensor([0, 2, 3, 4, 5] ,dtype = torch.long)#["<GO>", "B", "C", "D", "E"]
     decode_y = torch.tensor([2, 3, 4, 5, 0],dtype = torch.long)#["B", "C", "D", "E", "<eos>"]
     Seq2Seq_test = Seq2Seq()
     # loss = Seq2Seq_test(encode_x.unsqueeze(0), decode_x.unsqueeze(0), decode_y.unsqueeze(0))
